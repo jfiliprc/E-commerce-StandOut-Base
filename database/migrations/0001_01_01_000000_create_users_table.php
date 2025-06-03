f4879452d02bd1d08f742a1877131a871f3659ff
jfiliprc <felipvsrochaconst@gmail.com>
2025-06-03 11:56:09 -0300

    feat: adiciona campos e parametros na migration padrão 'users'

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,10 +12,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone', 20)->nullable();
+            $table->enum('role', ['cliente', 'admin'])->default('cliente');
+            $table->string('address')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->enum('status', ['ativo', 'inativo', 'bloqueado'])->default('ativo');
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
